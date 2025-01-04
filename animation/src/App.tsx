619b33421270059d68349081bb8efaec10bea536
Rohan <315scisyb2020rohanmishra@gmail.com>
2025-01-04 19:42:39 +0530

    added the hero section

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
+
+      <section className="z-0 min-h-screen bg-blue-500" />
     </div>
   )
 }
--- animation/src/components/Hero.tsx
+++ animation/src/components/Hero.tsx
@@ -1,36 +1,106 @@
-import { useRef, useState } from "react"
-import Button from "./Button"
-import { TiLocationArrow } from "react-icons/ti"
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { TiLocationArrow } from "react-icons/ti";
+import { useEffect, useRef, useState } from "react";
+
+import Button from "./Button";
+import { useGSAP } from "@gsap/react";
 
 
-function Hero() {
-  const [currentIndex, setCurrentIndex] = useState(1)
-  const [hasClicked, setHasClicked] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
-  const [loadedVideos, setLoadedVideos] = useState(0)
+gsap.registerPlugin(ScrollTrigger);
 
-  const totalVideos = 3;
-  const nextVdRef = useRef(null)
+const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
 
+  const [loading, setLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
 
+  const totalVideos = 4;
+  const nextVdRef = useRef<HTMLVideoElement | null>(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loadedVideos === totalVideos - 1) {
+      setLoading(false);
+    }
+  }, [loadedVideos]);
 
   const handleMiniVdClick = () => {
     setHasClicked(true);
 
-    setCurrentIndex(upcomingVideoIndex);
+    setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
+  };
 
-  }
+ 
 
-  const handleVideoLoad = () => {
-    setLoadedVideos((prev) => prev + 1);
-  }
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => {
+            if (nextVdRef.current) {
+              nextVdRef.current.play();
+            }
+          },
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
 
-  const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
 
-  const getVideoSrc = (index: number) => `videos/hero-${index}.mp4 `;
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
+
+  const getVideoSrc = (index: number) => `videos/hero-${index}.mp4`;
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
-      
+      {loading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
 
       <div
         id="video-frame"
@@ -38,7 +108,7 @@ function Hero() {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-         
+           
               <div
                 onClick={handleMiniVdClick}
                 className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
